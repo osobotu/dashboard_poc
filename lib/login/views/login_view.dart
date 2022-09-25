52d7e105c3da3f5ccf5fda0eaab0e362237fbd3b
Stephen Obotu Oduh <osobotu@gmail.com>
2022-09-25 09:41:27 +0100

    added mobile view of login view. Hosted project on firebase

--- lib/login/views/login_view.dart
+++ lib/login/views/login_view.dart
@@ -1,10 +1,28 @@
 import 'package:dashboard_poc/login/cubit/auth_cubit.dart';
+import 'package:dashboard_poc/responsive/responsive_layout.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
 
 class LoginView extends StatelessWidget {
   const LoginView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const ResponsiveLayout(
+      mobileLayout: MobileLoginView(),
+      desktopLayout: DesktopLoginView(),
+      tabletLayout: Scaffold(
+        backgroundColor: Colors.amberAccent,
+      ),
+    );
+  }
+}
+
+class DesktopLoginView extends StatelessWidget {
+  const DesktopLoginView({
+    Key? key,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -23,6 +41,7 @@ class LoginView extends StatelessWidget {
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisSize: MainAxisSize.min,
                 children: [
                   const Icon(
                     Icons.dashboard,
@@ -81,7 +100,8 @@ class LoginView extends StatelessWidget {
                           return ElevatedButton(
                             style: ElevatedButton.styleFrom(
                               primary: Colors.deepPurple,
-                              minimumSize: const Size(320, 60),
+                              minimumSize:
+                                  Size(MediaQuery.of(context).size.width, 60),
                             ),
                             onPressed: () async {
                               await context.read<AuthCubit>().login(
@@ -100,6 +120,124 @@ class LoginView extends StatelessWidget {
                   ),
                 ],
               ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class MobileLoginView extends StatelessWidget {
+  const MobileLoginView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    final emailController = TextEditingController();
+    final passwordController = TextEditingController();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SingleChildScrollView(
+        reverse: true,
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 40),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                const SizedBox(
+                  height: 200,
+                ),
+                const Icon(
+                  Icons.dashboard,
+                  color: Colors.deepPurple,
+                ),
+                Text(
+                  'Bellum',
+                  style: theme.textTheme.headline5,
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                Text(
+                  'Enter your email and password below',
+                  style: theme.textTheme.caption,
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                TextFormField(
+                  controller: emailController,
+                  decoration: InputDecoration(
+                    label: const Text('Email'),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                    hoverColor: Colors.deepPurpleAccent,
+                  ),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                TextFormField(
+                  obscureText: true,
+                  controller: passwordController,
+                  decoration: InputDecoration(
+                    label: const Text('Password'),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                    hoverColor: Colors.deepPurpleAccent,
+                  ),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                BlocConsumer<AuthCubit, AuthState>(
+                  listener: ((context, state) {
+                    if (state is AuthSuccess) {
+                      context.go('/home');
+                    } else if (state is AuthFailure) {
+                      showDialog(
+                          context: context,
+                          builder: (context) {
+                            return const Align(
+                              alignment: Alignment.bottomCenter,
+                              child: AlertDialog(
+                                title: Text(
+                                    'Incorrect Email or Password. Use test details.'),
+                              ),
+                            );
+                          });
+                    }
+                  }),
+                  builder: (context, state) {
+                    return Builder(
+                      builder: (context) {
+                        return ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            primary: Colors.deepPurple,
+                            minimumSize:
+                                Size(MediaQuery.of(context).size.width, 60),
+                          ),
+                          onPressed: () async {
+                            await context.read<AuthCubit>().login(
+                                email: emailController.text,
+                                password: passwordController.text);
+                          },
+                          child: state is AuthLoading
+                              ? const CircularProgressIndicator(
+                                  color: Colors.white,
+                                )
+                              : const Text('Log In'),
+                        );
+                      },
+                    );
+                  },
+                ),
+              ],
             ),
           ),
         ),
---
+++ lib/responsive/responsive_layout.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class ResponsiveLayout extends StatelessWidget {
+  const ResponsiveLayout({
+    Key? key,
+    required this.desktopLayout,
+    required this.mobileLayout,
+    required this.tabletLayout,
+  }) : super(key: key);
+
+  final Widget mobileLayout;
+  final Widget tabletLayout;
+  final Widget desktopLayout;
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(builder: (context, constraints) {
+      if (constraints.maxWidth < 480) {
+        return mobileLayout;
+      } else if (constraints.maxWidth < 1100) {
+        return tabletLayout;
+      } else {
+        return desktopLayout;
+      }
+    });
+  }
+}
